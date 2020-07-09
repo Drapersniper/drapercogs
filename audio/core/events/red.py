223da87da25f479ae1b55530e2f2503edcc66557
Drapersniper <27962761+drapersniper@users.noreply.github.com>
2020-07-09 16:59:33 +0100

    ....

--- audio/core/events/red.py
+++ audio/core/events/red.py
@@ -11,8 +11,7 @@ log = logging.getLogger("red.cogs.Audio.cog.Events.red")
 class RedEvents(MixinMeta, metaclass=CompositeMetaClass):
     @commands.Cog.listener()
     async def on_red_api_tokens_update(
-        self, service_name: str,
-            api_tokens: Mapping[str, str]
+        self, service_name: str, api_tokens: Mapping[str, str]
     ) -> None:
         if service_name == "youtube":
             self.api_interface.youtube_api.update_token(api_tokens)
