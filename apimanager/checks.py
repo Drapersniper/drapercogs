b91ca4a726c46e77aa8f2c8f16466267370ca1dd
Drapersniper <27962761+drapersniper@users.noreply.github.com>
2020-09-27 13:26:08 +0100

    ffs

--- apimanager/checks.py
+++ apimanager/checks.py
@@ -26,15 +26,13 @@ async def is_api_contributor(ctx: commands.Context):
     return api_user.can_post
 
 
-def is_api_mod():
-    async def predicate(ctx: commands.Context):
-        api_user = await API.get_user(cog=ctx.cog, member=ctx.author)
-        ctx.audio_api_user = api_user
-        if not api_user:
-            return False
-        return api_user.can_delete
+def is_api_mod(ctx: commands.Context):
+    api_user = await API.get_user(cog=ctx.cog, member=ctx.author)
+    ctx.audio_api_user = api_user
+    if not api_user:
+        return False
+    return api_user.can_delete
 
-    return commands.check(predicate)
 
 
 async def is_api_admin(ctx: commands.Context):
